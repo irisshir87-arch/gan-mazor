2db2a678cafee69cccd98512461cafebad11b86f
irisshir87-arch <irisshir87@gmail.com>
2026-07-26 12:09:51 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // בשלב זה האפליקציה עובדת במצב דמו מקומי.
 window.GAN_MAZOR_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
+  SUPABASE_URL: "https://clpwqnzccufjmcsiiwer.supabase.co/rest/v1/",
+  SUPABASE_ANON_KEY: "sb_publishable_0znGHPjUw1cBegtRl7fFsg_davc5JI2",
   KINDERGARTEN_SLUG: "gan-mazor"
 };

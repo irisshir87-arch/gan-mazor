932bf0d030de0a8d61691ae7301ce7f25c5d6575
irisshir87-arch <irisshir87@gmail.com>
2026-07-26 12:20:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // בשלב זה האפליקציה עובדת במצב דמו מקומי.
 window.GAN_MAZOR_CONFIG = {
-  SUPABASE_URL: "https://clpwqnzccufjmcsiiwer.supabase.co/rest/v1/",
+  SUPABASE_URL: "SUPABASE_URL: "https://clpwqnzccufjmcsiiver.supabase.co",",
   SUPABASE_ANON_KEY: "sb_publishable_0znGHPjUw1cBegtRl7fFsg_davc5JI2",
   KINDERGARTEN_SLUG: "gan-mazor"
 };

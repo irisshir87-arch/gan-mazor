bd41af50306a5d4eeee7e771eb60257f6014d3dc
irisshir87-arch <irisshir87@gmail.com>
2026-07-26 13:25:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // הגדרות החיבור של אפליקציית גן מזור
 window.GAN_MAZOR_CONFIG = {
-  SUPABASE_URL: "https://clpwqnzccufjmcsiiver.supabase.co",
+ SUPABASE_URL: "https://clpwqnzccufjmcsiiwer.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_0znGHPjUw1cBegtRl7fFsg_davc5JI2",
   KINDERGARTEN_SLUG: "gan-mazor"
 };

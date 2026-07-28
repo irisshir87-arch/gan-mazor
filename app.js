8c470de587899a70d714f4a266e2ffff3de27b6b
irisshir87-arch <irisshir87@gmail.com>
2026-07-28 22:54:56 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -738,7 +738,7 @@
   });
 
   if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => navigator.serviceWorker.register("sw.js?v=20260728-11").catch(console.warn));
+    window.addEventListener("load", () => navigator.serviceWorker.register("sw.js?v=20260728-12").catch(console.warn));
   }
 
   initialize();
